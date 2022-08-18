903979650d99985a711f33f7d0f62063e514e3fb
Paul Smith <paul@adhocteam.us>
2022-08-18 12:16:33 -0500

    Add pico.css to the scaffold
    Add a minimal CSS framework to the generated scaffold site. Goal is to
    provide a pleasing experience for new users, but also entice them to
    start editing and improve it.

--- main.go
+++ main.go
@@ -150,7 +150,13 @@ func (n *newCmd) do() error {
 		}
 	}
 
-	for _, name := range []string{"layouts/default.pushup", "pages/index.pushup", "static/pushup.css", "pkg/app.go"} {
+	scaffoldFiles := []string{"layouts/default.pushup",
+		"pages/index.pushup",
+		"static/pico.min.css",
+		"static/custom.css",
+		"pkg/app.go",
+	}
+	for _, name := range scaffoldFiles {
 		dest := filepath.Join(n.projectDir, "app", name)
 		src := filepath.Join("scaffold", name)
 		if err := copyFileFS(scaffold, dest, src); err != nil {
