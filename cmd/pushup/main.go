5ccf53eb245923331e9b2fd5f983b8c8043b55cb
Paul Smith <paul@adhocteam.us>
2023-12-15 10:41:59 -0600

    Rollback last change

--- cmd/pushup/main.go
+++
@@ -1,9 +0,0 @@
-// this file is mechanically generated, do not edit!
-package main
-
-import "github.com/adhocteam/pushup/internal/pushup"
-import "github.com/adhocteam/pushup/api"
-
-func main() {
-	api.Main(pushup.Router)
-}
