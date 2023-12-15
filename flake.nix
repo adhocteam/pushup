94bd160f31de0641a209d79b7c2b8749effa11a4
Paul Smith <paul@adhocteam.us>
2023-12-15 11:20:06 -0600

    nix: make Pushup a proper flake
    This change adds packages to the Nix flake such that Pushup can now be
    added to systems using Nix as a package manager.
    
    From the local checked-out repository, running `nix build .#` or simply
    `nix build` will build Pushup and the binary executable will be in
    `./result/bin/pushup`.
    
    To just run Pushup without checking out the repository, you can now do
    `nix run github:adhocteam/pushup`, passing arguments as necessary. All
    the normal Nix flake commands and capabilities are now available.

--- flake.nix
+++ flake.nix
@@ -1,15 +1,42 @@
 {
   description = "Pushup is a new web framework for Go";
 
-  inputs.nixpkgs.url = "nixpkgs/nixos-unstable";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
 
   outputs = { self, nixpkgs }:
     let
+      version = "0.0.1"; # TODO(paulsmith): source from version.go
       forEachSystem = fn:
         nixpkgs.lib.genAttrs [ "x86_64-linux" "aarch64-darwin" "aarch64-linux" ]
-        (system: fn nixpkgs.legacyPackages.${system});
+        (system:
+          fn {
+            pkgs = nixpkgs.legacyPackages.${system};
+            inherit system;
+          });
     in {
-      devShells = forEachSystem (pkgs: {
+      packages = forEachSystem ({ pkgs, system }: {
+        default = pkgs.buildGoModule rec {
+          pname = "pushup";
+          inherit version;
+          src = ./.;
+          vendorHash = null;
+          subPackages = ".";
+          CGO_ENABLED = 0;
+          doCheck = false;
+          nativeBuildInputs = with pkgs; [ makeWrapper ];
+          allowGoReference = true;
+          postInstall = ''
+            wrapProgram $out/bin/${pname} --prefix PATH : ${pkgs.lib.makeBinPath (with pkgs; [go])}
+          '';
+          meta = with nixpkgs.lib; {
+            description = "Pushup web framework for Go";
+            homepage = "https://pushup.adhoc.dev/";
+            license = licenses.mit;
+          };
+        };
+      });
+
+      devShells = forEachSystem ({ pkgs, ... }: {
         default =
           pkgs.mkShell { buildInputs = with pkgs; [ go_1_20 gotools gopls ]; };
       });
