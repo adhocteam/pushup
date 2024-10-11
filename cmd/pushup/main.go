191b3972dc7cbac3956680b488b0faac1daadcc1
Paul Smith <paul@adhocteam.us>
2024-10-11 15:39:57 -0500

    cmd: Scaffold build subcommand

--- cmd/pushup/main.go
+++ cmd/pushup/main.go
@@ -1,7 +1,79 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"os"
+
+	"github.com/adhocteam/pushup/internal"
+)
+
+type command struct {
+	name  string
+	setup func(*flag.FlagSet)
+	run   func(*flag.FlagSet) error
+}
+
+var commands = []command{
+	{
+		name: "build",
+		setup: func(fs *flag.FlagSet) {
+			fs.String("root", ".", "Root directory")
+		},
+		run: func(fs *flag.FlagSet) error {
+			root := fs.Lookup("root").Value.String()
+			return internal.Build(root)
+		},
+	},
+}
 
 func main() {
-	fmt.Println("Hello, ^Pushup.")
+	if len(os.Args) < 2 {
+		fmt.Println("Usage: pushup <command>")
+		os.Exit(1)
+	}
+
+	cmdName := os.Args[1]
+	cmd := findCommand(cmdName)
+	if cmd == nil {
+		fmt.Printf("Unknown command: %s\n", cmdName)
+		printUsage()
+		os.Exit(1)
+	}
+
+	fs := flag.NewFlagSet(cmdName, flag.ExitOnError)
+	cmd.setup(fs)
+	fs.Usage = func() {
+		fmt.Printf("Usage: pushup %s [flags]\n", cmdName)
+		fs.PrintDefaults()
+	}
+
+	err := fs.Parse(os.Args[2:])
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	err = cmd.run(fs)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
+
+func findCommand(name string) *command {
+	for i := range commands {
+		if commands[i].name == name {
+			return &commands[i]
+		}
+	}
+	return nil
+}
+
+func printUsage() {
+	fmt.Println("Usage: pushup <command>")
+	fmt.Println("Commands:")
+	for _, cmd := range commands {
+		fmt.Printf("  %s\n", cmd.name)
+	}
 }
---
+++ internal/build.go
@@ -0,0 +1,8 @@
+package internal
+
+import "fmt"
+
+func Build(rootDir string) error {
+	fmt.Printf("Building: %s\n", rootDir)
+	return nil
+}
