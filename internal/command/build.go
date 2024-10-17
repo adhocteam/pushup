9bd8a3a21da3cd91a341e90297d592d22436f257
Paul Smith <paul@adhocteam.us>
2024-10-17 13:58:18 -0500

    build: Traverse root for .up files

--- internal/command/build.go
+++ internal/command/build.go
@@ -1,11 +1,34 @@
 package command
 
 import (
-	"fmt"
-	"os"
+	"io/fs"
+	"log/slog"
+	"path/filepath"
 )
 
-func Build(rootDir string) error {
-	fmt.Fprintf(os.Stderr, "\x1b[33mBuilding: %s\x1b[0m\n", rootDir)
+func Build(root string) error {
+	// TODO: take a logger optionally from the caller
+	logger := slog.Default()
+	logger.Info("Building", "root", root)
+
+	findUpFiles(root, func(path string) error {
+		logger.Info("Found .up file", "path", path)
+		return nil
+	})
+
 	return nil
+}
+
+func findUpFiles(root string, callback func(string) error) error {
+	return filepath.WalkDir(root, func(path string, d fs.DirEntry, err error) error {
+		if err != nil {
+			return err
+		}
+
+		if !d.IsDir() && filepath.Ext(path) == ".up" {
+			return callback(path)
+		}
+
+		return nil
+	})
 }
